e8ecaffc4be403b7adecc9988664f46f84c3a727
Dami Drevid <oludre@gmail.com>
2022-01-29 02:10:14 +0100

    Class objects

--- ClassAndObjectchallenge/src/com/netraenis/Main.java
+++ ClassAndObjectchallenge/src/com/netraenis/Main.java
@@ -3,6 +3,13 @@ package com.netraenis;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BankAccount bank = new BankAccount();
+
+        System.out.println("Opening balance is " + bank.getAccountBalance());
+        bank.deposit(5000);
+        System.out.println("Your current balance is " + bank.getAccountBalance());
+        bank.withdraw(50 00.01);
+        System.out.println("Your current balance is " + bank.getAccountBalance());
+
     }
 }
