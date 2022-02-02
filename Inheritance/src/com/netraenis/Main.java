43b0421736aebfba2d60391a1dda6a0c7ec996ad
Dami Drevid <oludre@gmail.com>
2022-02-02 04:01:41 +0100

    Static vs Instance Record

---
+++ Inheritance/src/com/netraenis/Main.java
@@ -0,0 +1,19 @@
+package com.netraenis;
+
+
+
+public class Main {
+
+    //For Non-Static, i.e instance method call
+//    public static void main(String[] args) {
+//        Tester tester = new Tester();
+//        tester.tastesGood();
+//
+//
+//    }
+
+    // Static method call
+    public static void main(String[] args) {
+       Tester.tastesGood();
+    }
+}
