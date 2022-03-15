6aa479c1eb1fb86fddcd14346ee11f22da094de6
Dami Drevid <oludre@gmail.com>
2022-03-15 05:32:53 +0100

    ArrayList challenge

---
+++ ArrayListChallenge/src/com/netraenis/Contact.java
@@ -0,0 +1,30 @@
+package com.netraenis;
+
+import java.util.ArrayList;
+
+public class Contact {
+    private ArrayList<String> contactArray = new ArrayList<String>();
+
+    //Add contact
+    public void saveContact(String name) {
+        if (userExists(name)){
+            System.out.println("User name: " + name + " already exists");
+        } contactArray.add(name);
+    }
+
+    public void editUser(int position, String newName) {
+        contactArray.set(position, newName);
+    }
+
+    public boolean userExists(String name){
+        int searchIndex = contactArray.indexOf(name);
+        if (searchIndex >= 0){
+            return true;
+        } return false;
+
+    }
+
+
+
+
+}
