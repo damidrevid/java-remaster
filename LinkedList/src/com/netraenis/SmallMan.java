479b9c85f205dc440b9f63c533e6f6facfa26841
Dami Drevid <oludre@gmail.com>
2022-03-27 22:14:20 +0100

    human interface

--- LinkedList/src/com/netraenis/SmallMan.java
+++ LinkedList/src/com/netraenis/SmallMan.java
@@ -2,12 +2,22 @@ package com.netraenis;
 
 import java.util.ArrayList;
 
-public class SmallMan<Organs> {
+public class SmallMan<Organs> implements Human {
     private int age;
     private ArrayList<Organs> theOrgan;
 
     public SmallMan(int age) {
         this.age = age;
         this.theOrgan = new ArrayList<Organs>();
+    }
+
+    @Override
+    public void race() {
+
+    }
+
+    @Override
+    public void colour() {
+
     }
 }
