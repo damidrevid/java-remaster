70e675f862eb3ef75596a581fb1405ea577f02c1
Dami Drevid <oludre@gmail.com>
2022-04-08 21:44:58 +0100

    Escrow

---
+++ AbstractChallenge/src/com/netraenis/Escrow.java
@@ -0,0 +1,29 @@
+package com.netraenis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Escrow implements User {
+    private List<Customer> customers;
+    private double amount;
+
+    public Escrow(double amount){
+        this.amount = amount;
+        this.customers = new ArrayList<>();
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+
+    @Override
+    public void saveUser() {
+
+    }
+
+    @Override
+    public void searchUser() {
+
+    }
+}
